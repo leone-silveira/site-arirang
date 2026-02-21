1583d8f62373e33225f62bcb2eaa4c3729a08fc2
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2026-02-20 22:56:50 -0400

    fix: Link instead of a

--- arirang-site/app/components/Navbar.tsx
+++ arirang-site/app/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Navbar() {
   return (
     <nav className="flex justify-between items-center p-4 shadow">
@@ -6,16 +8,16 @@ export default function Navbar() {
       </h1>
 
       <div className="flex gap-6">
-        <a href="/">Home</a>
-        <a href="/cursos">Cursos</a>
-        <a href="/sobre">Sobre</a>
-        <a href="/contato">Contato</a>
-        <a
+        <Link href="/">Home</Link>
+        <Link href="/cursos">Cursos</Link>
+        <Link href="/sobre">Sobre</Link>
+        <Link href="/contato">Contato</Link>
+        <Link
           href="/login"
           className="bg-red-600 text-white px-4 py-2 rounded"
         >
           Login
-        </a>
+        </Link>
       </div>
     </nav>
   );
