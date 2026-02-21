a4aaff757df65810af3711caeae93f98e25198ac
Leone Raulino Silveira <leone.zordenoni@gmail.com>
2026-02-21 10:17:09 -0400

    fix: next config and prisma

--- arirang-site/lib/prisma.ts
+++ arirang-site/lib/prisma.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from "@prisma/client/extension";
 
 const globalForPrisma = global as unknown as {
-  prisma: PrismaClient;
+  prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ||
+  globalForPrisma.prisma ??
   new PrismaClient({
-    log: ["query"],
+    log: ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
